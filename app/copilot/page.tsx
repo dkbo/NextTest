4d6b683479c86b608628b807023ae06a8e0740d6
LEO <leo_lu@ch-si.com.tw>
2025-07-31 16:54:24 +0800

    feat: 串接智慧AI

--- app/copilot/page.tsx
+++ app/copilot/page.tsx
@@ -4,7 +4,7 @@ export default function CopilotPage() {
   return (
     <>
       <iframe
-        src="https://copilotstudio.microsoft.com/environments/Default-c0b8e2ad-56b1-4066-bb61-6e83109289fa/bots/crfb5_BlBhhzHK4yl6hge2Ek52J/canvas?__version__=2&enableFileAttachment=true"
+        src="https://copilotstudio.microsoft.com/environments/Default-c0b8e2ad-56b1-4066-bb61-6e83109289fa/bots/crfb5_BlBhhzHK4yl6hge2Ek52J/webchat?__version__=2"
         width="100%"
         height="100vh"
         className="border-none h-[calc(100vh-60px)]"
